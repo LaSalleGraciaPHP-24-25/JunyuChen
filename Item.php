dafc5aeac0cc26efca97de02fd3c0a90fbeb4ef1
xiaoyuu857 <junyuchen13@gmail.com>
2024-10-04 18:46:15 +0200

    changes to Item.php

---
+++ Item.php
@@ -0,0 +1,51 @@
+<?php
+
+abstract class Item implements Naming {
+    protected $name;
+    protected $weight;
+    protected $price;
+    protected $isNew;
+
+    protected function __construct(string $name, float $weight, float $price, bool $isNew) {
+        $this->name = $name;
+        $this->weight = $weight;
+        $this->price = $price;
+        $this->isNew = $isNew;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName(string $name) {
+        $this->name = $name;
+    }
+
+    public function getWeight() {
+        return $this->weight;
+    }
+
+    public function setWeight(float $weight) {
+        $this->weight = $weight;
+    }
+
+    public function getPrice() {
+        return $this->price;
+    }
+
+    public function setPrice(float $price) {
+        $this->price = $price;
+    }
+
+    public function getIsNew() {
+        return $this->isNew;
+    }
+
+    public function setIsNew(bool $isNew) {
+        $this->isNew = $isNew;
+    }
+
+    public function calcPriceWithTax() {
+        return $this->price * 1.21;
+    }
+}
